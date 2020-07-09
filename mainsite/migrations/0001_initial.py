c53559bf43f9e8752f46964432639ed1650a1ca7
smalljia <leesmalljia@gmail.com>
2020-07-09 20:21:34 +0800

    FrameandPrintblue

---
+++ mainsite/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('pub_date', models.DateTimeField(default=datetime.datetime(2020, 7, 9, 10, 41, 14, 695929, tzinfo=utc))),
+            ],
+            options={
+                'ordering': ('-pub_date',),
+            },
+        ),
+    ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+def showpost(request,slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
